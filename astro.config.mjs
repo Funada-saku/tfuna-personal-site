5fc6601e80cc0197508c658b69406d77cf78aa02
tfunada <cityofdarkness19021319@gmail.com>
2026-08-25 20:20:14 +0900

    Prepare public GitHub Pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://funada-saku.github.io/workspace/",
-  base: "/workspace/",
+  site: "https://funada-saku.github.io/tfuna-personal-site/",
+  base: "/tfuna-personal-site/",
   output: "static"
 });
